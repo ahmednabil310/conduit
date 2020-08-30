169770ac115cab0b7946fb084ab65bce735da59f
Ahmed <an30458@gmail.com>
2020-08-30 13:34:47 +0200

    Updates

---
+++ precache-manifest.b2c7f54ae68c4b1a5de7f7f691e9ef08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a0d1cd296b3071f8e5b7b7894a8fab",
+    "url": "/conduit/index.html"
+  },
+  {
+    "revision": "f0347d4eaa4063ee3345",
+    "url": "/conduit/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "a769d217d2e94493dbbe",
+    "url": "/conduit/static/css/main.48d3a20f.chunk.css"
+  },
+  {
+    "revision": "f0347d4eaa4063ee3345",
+    "url": "/conduit/static/js/2.9a151800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conduit/static/js/2.9a151800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a769d217d2e94493dbbe",
+    "url": "/conduit/static/js/main.999b1311.chunk.js"
+  },
+  {
+    "revision": "b3a276d73fbb29001ed7",
+    "url": "/conduit/static/js/runtime-main.b1eda6c5.js"
+  }
+]);
